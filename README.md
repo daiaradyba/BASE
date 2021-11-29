cec79d60eda43165a17a67bc3d0a72dbbc394ab8
daiaradyba <58401153+daiaradyba@users.noreply.github.com>
2021-11-29 16:22:52 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# BASE
+# collisionAlgorithm
+Algorithm to detect collision between two objects
---
+++ sketch.js
@@ -0,0 +1,13 @@
+
+function setup() {
+  createCanvas(1200,800);
+ 
+
+}
+
+function draw() {
+  background(0,0,0);  
+ 
+  drawSprites();
+}
+
